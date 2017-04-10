77eef50d7b9ab79940254e2a54a2f75fae59ed9c
Paul Baca <pbaca110@cnm.edu>
2017-04-10 11:06:36 -0600

    committed

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> 	hi im paul </title>
+	</head>
+	<body>
+		<p>I like cars and working on them im a pretty easy going dude. I also like to skateboard and bmx.
+		really not much to know about me lol.</p>
+	</body>
+</html>
+<img src= "images/lol.jpg alt="" />
